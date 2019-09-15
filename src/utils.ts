6776f6871c53f8d61d68deb5b385f316e5d94288
Raphaël Thériault <raphael_theriault@outlook.com>
2019-09-15 19:31:35 -0400

    Download progress

--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,21 @@
 import { promises as fs } from "fs";
 import fetch, { Response } from "node-fetch";
 import * as unzipper from "unzipper";
+import * as vscode from "vscode";
 
 export async function downlaodAndUnzip(url: string, path: string) {
-    const res: Response = await fetch(url);
-    res.body.pipe(unzipper.Extract({path}));
+    await vscode.window.withProgress(
+        {
+            title: `Downloading ${url}...`,
+            location: vscode.ProgressLocation.Notification,
+            cancellable: false,
+        },
+        async (progress) => {
+                const res: Response = await fetch(url);
+                progress.report({message: "Extracting..."});
+                res.body.pipe(unzipper.Extract({path}));
+        },
+    );
 }
 
 export async function directoryIsEmpty(path: string): Promise<boolean> {
