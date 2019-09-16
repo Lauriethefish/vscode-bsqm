1cca8c40dd81d44bb35bfed1a8c7b97c7fc0675a
Raphaël Thériault <raphael_theriault@outlook.com>
2019-09-15 22:27:40 -0400

    Started create command

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,12 @@
 import * as vscode from "vscode";
 import configure from "./configure";
+import create from "./create";
 
 export function activate(context: vscode.ExtensionContext) {
     const commands: {[name: string]: vscode.Disposable} = {};
 
     commands.configure = vscode.commands.registerCommand("bsqm.configure", configure);
+    commands.create = vscode.commands.registerCommand("bsqm.create", create);
 
     Object.keys(commands).forEach((command) => {
         context.subscriptions.push(commands[command]);
